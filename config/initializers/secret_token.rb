b1db0af346b263d80d5d3081eecad5e2ed835ffc
Developer <developer@Developers-iMac.local>
2013-11-21 09:31:14 -0500

    - Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aceldev::Application.config.secret_token = '4c7f7db548f72b7b5d5319be5a7b1ad8015a79b5f321edc8635c95b572e392ef89097f0f8326c3ff23097567db27f2829439a3aba81d8faefea68b0bb15ff6ca'
